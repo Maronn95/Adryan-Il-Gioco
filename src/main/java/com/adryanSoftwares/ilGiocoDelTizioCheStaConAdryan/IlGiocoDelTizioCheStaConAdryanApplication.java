9450b2ae7c8a1ecac5a547aae80f67b60b49f2cf
Maronn95 <73346150+Maronn95@users.noreply.github.com>
2021-11-14 19:44:09 +0100

    start

---
+++ src/main/java/com/adryanSoftwares/ilGiocoDelTizioCheStaConAdryan/IlGiocoDelTizioCheStaConAdryanApplication.java
@@ -0,0 +1,14 @@
+package com.adryanSoftwares.ilGiocoDelTizioCheStaConAdryan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlGiocoDelTizioCheStaConAdryanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlGiocoDelTizioCheStaConAdryanApplication.class, args);
+		System.out.print("shao");
+	}
+
+}
---
+++ src/test/java/com/adryanSoftwares/ilGiocoDelTizioCheStaConAdryan/IlGiocoDelTizioCheStaConAdryanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adryanSoftwares.ilGiocoDelTizioCheStaConAdryan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlGiocoDelTizioCheStaConAdryanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

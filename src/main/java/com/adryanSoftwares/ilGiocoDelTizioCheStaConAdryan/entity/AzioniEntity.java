2a027d5f90293e80d40f808b425d809e18998367
Maronn95 <73346150+Maronn95@users.noreply.github.com>
2021-11-26 17:49:18 +0100

    Classi parte 1,76

--- src/main/java/com/adryanSoftwares/ilGiocoDelTizioCheStaConAdryan/entity/AzioniEntity.java
+++ src/main/java/com/adryanSoftwares/ilGiocoDelTizioCheStaConAdryan/entity/AzioniEntity.java
@@ -5,13 +5,20 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+@Setter
+@Getter
 @Entity
 public class AzioniEntity {
-    @Setter
-    @Getter
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
 
+    private String name;
+
+    public AzioniEntity(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
--- src/main/java/com/adryanSoftwares/ilGiocoDelTizioCheStaConAdryan/entity/PgEntity.java
+++ src/main/java/com/adryanSoftwares/ilGiocoDelTizioCheStaConAdryan/entity/PgEntity.java
@@ -2,6 +2,7 @@ package com.adryanSoftwares.ilGiocoDelTizioCheStaConAdryan.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.bind.Name;
 
 import javax.persistence.*;
 
@@ -9,9 +10,16 @@ import javax.persistence.*;
 @Getter
 @Setter
 public class PgEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
     private Long id;
 
+    private String name;
+
+    public PgEntity(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }

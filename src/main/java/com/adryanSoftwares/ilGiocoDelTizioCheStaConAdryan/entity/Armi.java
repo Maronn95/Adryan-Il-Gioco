623ee56e2d26b718377c65e60b828d851e267909
Maronn95 <73346150+Maronn95@users.noreply.github.com>
2022-04-24 19:26:05 +0200

    Armi zezze

--- src/main/java/com/adryanSoftwares/ilGiocoDelTizioCheStaConAdryan/entity/Armi.java
+++ src/main/java/com/adryanSoftwares/ilGiocoDelTizioCheStaConAdryan/entity/Armi.java
@@ -7,43 +7,68 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class Armi {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "ArmiId")
-    private Long ArmiId;
+    private Integer armiId;
 
-    @Column(name="Descrizione")
+    private String name;
+
     private String descrizione;
 
-    @Column(name="ArmaADistanza")
     private Boolean armaADistanza;
 
-    @Column(name="DannoTipo")
     private String dannoTipo;
 
-    @Column(name="Danno")
     private Double danno;
 
-    @Column(name="Gittata")
     private Double gittata;
 
-    @Column(name="AttaccoMultiplo")
-    private Boolean attaccoMultiplo;
-
-    @Column(name="ProbabilitaAttaccoMultiplo")
-    private Double probabilitaAttaccoMultiplo;
-
-    @Column(name="MoltiplicatoreCritico")
     private Double moltiplicatoreCritico;
 
-    @Column(name="ProbabilitaCriticoArma")
     private Double probabilitaCriticoArma;
 
+    public Armi(Integer armiId) {
+
+       switch (armiId){
+           case 1:
+               this.armiId=1;
+               this.name="Spada Zezza";
+               this.descrizione="È proprio una spada de merda";
+               this.armaADistanza=false;
+               this.dannoTipo="Tagliente";
+               this.danno=10.0;
+               this.gittata=1.0;
+               this.moltiplicatoreCritico=1.5;
+               this.probabilitaCriticoArma=20.0;
+               break;
+
+            case 2:
+                this.armiId=2;
+                this.name="Martello Zezzo";
+                this.descrizione="Proprio un martello de merda";
+                this.armaADistanza=false;
+                this.dannoTipo="Contundente";
+                this.danno=12.0;
+                this.gittata=1.0;
+                this.moltiplicatoreCritico=1.5;
+                this.probabilitaCriticoArma=21.0;
+                break;
+
+           case 3:
+               this.armiId=3;
+               this.name="Arco Zezzo";
+               this.descrizione="Non ci pii un cazzo con què";
+               this.armaADistanza=true;
+               this.dannoTipo="Perforante";
+               this.danno=6.0;
+               this.gittata=5.0;
+               this.moltiplicatoreCritico=1.2;
+               this.probabilitaCriticoArma=16.0;
+               break;
+        }
+    }
 }

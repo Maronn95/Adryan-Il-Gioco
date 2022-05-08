8783dfc2cea516ffcc15b83a58bd1c7768ac44b4
Maronn95 <danielenerichieti@gmail.com>
2022-05-08 17:24:42 +0200

    in questa parte del progetto non ho fatto una sega però volevo vedere se riuscivo a committare, yay.

--- src/main/java/com/adryanSoftwares/ilGiocoDelTizioCheStaConAdryan/servicesImpl/OggettoServiceImpl.java
+++ src/main/java/com/adryanSoftwares/ilGiocoDelTizioCheStaConAdryan/servicesImpl/OggettoServiceImpl.java
@@ -2,7 +2,6 @@ package com.adryanSoftwares.ilGiocoDelTizioCheStaConAdryan.servicesImpl;
 
 import com.adryanSoftwares.ilGiocoDelTizioCheStaConAdryan.entity.BattleGroundEntity;
 import com.adryanSoftwares.ilGiocoDelTizioCheStaConAdryan.entity.OggettiEntity;
-import com.adryanSoftwares.ilGiocoDelTizioCheStaConAdryan.repositorys.OggettiRepo;
 import com.adryanSoftwares.ilGiocoDelTizioCheStaConAdryan.services.OggettiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class OggettoServiceImpl implements OggettiService {
 
-    @Autowired
-    OggettiRepo oggettiRepo;
+
 
     @Override
     public void saveOrUpdate(OggettiEntity oggetto) {
